8c112af0ea5231a58af6c8d75ef088f4fa8f6fed
Johanna Eriksson <je224gs@student.lnu.se>
2024-09-02 14:29:32 +0200

    Code ready for first trial

--- greetingApp.js
+++ greetingApp.js
@@ -1,17 +1,34 @@
 const template = document.createElement('template')
 template.innerHTML = `
   <style>
-    <p>Type your name:</p>
-    <input type="text">
-    <button>Submit</button>
+    <form>
+      <p>Type your name:</p>
+      <input type="text">
+      <button>Submit</button>
+    </form>
   </style>`
 
 customElements.define('greeting-app',
   class extends HTMLElement {
+    #form
     constructor () {
       super ()
       this.attachShadow({mode: 'open'})
        .appendChild(template.content.cloneNode(true))
+
+      this.#form = this.shadowRoot.querySelector('form')
+      
+      this.#form.addEventListener('submit', e => {
+        e.preventDefault()
+        const name = this.shadowRoot.querySelector('input').value
+        this.#sendGreeting(name)
+      })
+    }
+
+    #sendGreeting(name) {
+      newTextElement = document.createElement('p')
+      newTextElement.textContent = `Hello, ${name}!`
+      this.#form.appendChild(newTextElement)
     }
   }
 )
